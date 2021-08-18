0be9de1200f94cde1c74c8fc47a51d3fd8e2440c
anubhavcu <anubhav342y@gmail.com>
2021-08-19 00:16:08 +0530

    added some basic routes and some dummy data
    - added some dummy data with some basic routes to /, /api/notes,
      /api/notes/:id
    - added a .gitignore file

---
+++ backend/data/notes.js
@@ -0,0 +1,44 @@
+const notes = [
+  {
+    _id: '1',
+    title: 'azure Crossing',
+    content:
+      'Molestiae temporibus magnam asperiores nulla. Iure quas eligendi quasi ipsam facere ullam aut rerum. Porro nemo repellat et aut voluptas consequatur.',
+    category: 'College',
+  },
+  {
+    _id: '2',
+    title: 'Berkshire Vermont',
+    content:
+      'Aut aut qui corrupti recusandae. Cupiditate tenetur vitae quas dolorum perferendis eum.',
+
+    category: 'Learning',
+  },
+  {
+    _id: '3',
+    title: 'Loan Belarussian virtual',
+    content: 'Sed doloremque ad repellendus a perspiciatis deleniti veritatis.',
+    category: 'other',
+  },
+
+  {
+    _id: '4',
+    title: 'Shilling',
+    content: 'Iure accusamus vel consequatur.',
+    category: 'College',
+  },
+  {
+    _id: '5',
+    title: 'solution-oriented Regional',
+    content: 'Suscipit esse culpa esse quia.',
+    category: 'Learning',
+  },
+  {
+    _id: '6',
+    title: ' microchip',
+    content: 'Veritatis assumenda fuga in accusamus consectetur neque.',
+    category: 'other',
+  },
+];
+
+module.exports = notes;
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const notes = require('./data/notes');
 
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('Api is running');
+});
+
+app.get('/api/notes', (req, res) => {
+  res.json(notes);
+});
+
+app.get('/api/notes/:id', (req, res) => {
+  const note = notes.find((item) => item._id === req.params.id);
+  res.json(note);
 });
 
 app.listen(5000, console.log('server started on port 5000'));
