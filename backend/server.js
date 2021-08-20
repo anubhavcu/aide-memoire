85ddf34e610290d7b1b670928eb54a2cedb8f1e4
anubhavcu <anubhav342y@gmail.com>
2021-08-20 14:39:40 +0530

    added dotenv package, some modifications in the .gitignore file

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const notes = require('./data/notes');
+const dotenv = require('dotenv');
 
 const app = express();
+
+dotenv.config();
+const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
   res.send('Api is running');
@@ -16,4 +20,4 @@ app.get('/api/notes/:id', (req, res) => {
   res.json(note);
 });
 
-app.listen(5000, console.log('server started on port 5000'));
+app.listen(PORT, console.log(`server started on port ${PORT}`));
