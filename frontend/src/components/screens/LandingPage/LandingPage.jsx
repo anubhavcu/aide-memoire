2a15f7419922a6344eff20edeb14d80a7ef3f8b5
anubhavcu <anubhav342y@gmail.com>
2021-08-30 12:54:37 +0530

    added some text and styles to landing page

--- frontend/src/components/screens/LandingPage/LandingPage.jsx
+++ frontend/src/components/screens/LandingPage/LandingPage.jsx
@@ -1,4 +1,4 @@
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Button } from 'react-bootstrap';
 import './LandingPage.css';
 
 const LandingPage = () => {
@@ -6,7 +6,32 @@ const LandingPage = () => {
     <div className='landingBackground'>
       <Container>
         <Row>
-          <Col>Landing Page</Col>
+          <Col>
+            <div className='intro-text'>
+              <h1 className='title'>Welcome to Notes App! </h1>
+              <p className='subtitle'>One safe place for all your notes..</p>
+            </div>
+          </Col>
+          <Row>
+            <Col>
+              <div className='intro-buttons'>
+                <Button
+                  className='mx-4 customButton'
+                  variant='primary '
+                  size='lg'
+                >
+                  Login
+                </Button>{' '}
+                <Button
+                  className='mx-2 customButton'
+                  variant='secondary'
+                  size='lg'
+                >
+                  Sign Up
+                </Button>{' '}
+              </div>
+            </Col>
+          </Row>
         </Row>
       </Container>
     </div>
