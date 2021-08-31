76443bb32c6de1b3897ae98b21ce69ec39f7b988
anubhavcu <anubhav342y@gmail.com>
2021-08-31 09:48:12 +0530

    added MainScreen component
    - MainScreen component will act as a framework in which we will pass
      contents and header
    - added some css for the MainScreen component

--- frontend/src/components/screens/Notes/Notes.jsx
+++ frontend/src/components/screens/Notes/Notes.jsx
@@ -1,5 +1,17 @@
+import { Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import MainScreen from '../../MainScreen';
+
 const Notes = () => {
-  return <div>Notes</div>;
+  return (
+    <MainScreen title='Welcome back Anubhav..' children='hello world'>
+      <Link to='/createnote'>
+        <Button style={{ marginLeft: 10, marginBottom: 6 }} size='lg'>
+          Create new Note
+        </Button>
+      </Link>
+    </MainScreen>
+  );
 };
 
 export default Notes;
