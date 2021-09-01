f47752f33507288dc602244ffddbb207b7803220
anubhavcu <anubhav342y@gmail.com>
2021-09-01 16:17:42 +0530

    connected mongoDB to backend

---
+++ backend/config/db.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      //       useCreateIndex: true,
+    });
+    console.log('database connected', conn.connection.host);
+  } catch (err) {
+    console.error(err.message);
+    process.exit();
+  }
+};
+
+module.exports = connectDB;
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const notes = require('./data/notes');
 const dotenv = require('dotenv');
+const connectDB = require('./config/db');
 
 const app = express();
 
 dotenv.config();
+connectDB();
 const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
