7ef5b4c710f33cf4458746d9d8884be2b3f7b0e8
anubhavcu <anubhav342y@gmail.com>
2021-09-01 11:21:37 +0530

    added body contents in Notes component
    - copied notes.js file temporarily for rendering notes, later it will be
      fetched from backend
    - added content for body for each note using Cards
    - added some styling to the cards

--- frontend/src/components/screens/Notes/Notes.jsx
+++ frontend/src/components/screens/Notes/Notes.jsx
@@ -1,8 +1,17 @@
-import { Button, Card } from 'react-bootstrap';
+import { Badge, Button, Card } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import MainScreen from '../../MainScreen';
+import { useEffect } from 'react';
+import notes from '../../../notes.js';
 
 const Notes = () => {
+  useEffect(() => {
+    console.log(notes);
+  }, []);
+  const handleDelete = (id) => {
+    if (window.confirm('Are you sure?')) {
+    }
+  };
   return (
     <MainScreen title='Welcome back Anubhav..' children='hello world'>
       <Link to='/createnote'>
@@ -10,28 +19,48 @@ const Notes = () => {
           Create new Note
         </Button>
       </Link>
-      <Card style={{ margin: 10 }}>
-        <Card.Header style={{ display: 'flex' }}>
-          <span
-            style={{
-              flex: 1,
-              color: 'black',
-              textDecoration: 'none',
-              alignSelf: 'center',
-              fontSize: 20,
-              cursor: 'pointer',
-            }}
-          >
-            title
-          </span>
-          <div>
-            <Button>Edit</Button>
-            <Button variant='danger' className='mx-2'>
-              Delete
-            </Button>
-          </div>
-        </Card.Header>
-      </Card>
+      {notes.map((note) => (
+        <Card style={{ margin: 10 }}>
+          <Card.Header style={{ display: 'flex' }}>
+            <span
+              style={{
+                flex: 1,
+                color: 'black',
+                textDecoration: 'none',
+                alignSelf: 'center',
+                fontSize: 20,
+                cursor: 'pointer',
+              }}
+            >
+              {note.title}
+            </span>
+            <div>
+              <Link to={`/note/${note._id}`}>
+                <Button>Edit</Button>
+              </Link>
+              <Button
+                variant='danger'
+                className='mx-2'
+                onClick={() => handleDelete(note._id)}
+              >
+                Delete
+              </Button>
+            </div>
+          </Card.Header>
+          <Card.Body>
+            <h4>
+              {/* <Badge variant='success'>Category -{note.category}</Badge> */}
+              <Button variant='success' style={{ borderRadius: 25 }}>
+                Category - {note.category}
+              </Button>
+            </h4>
+            <blockquote className='blockquote mb-0'>
+              <p>{note.content}</p>
+              <footer className='blockquote-footer'>Created on date</footer>
+            </blockquote>
+          </Card.Body>
+        </Card>
+      ))}
     </MainScreen>
   );
 };
@@ -39,3 +68,6 @@ const Notes = () => {
 export default Notes;
 
 // for flex : 1  - https://developer.mozilla.org/en-US/docs/Web/CSS/flex
+
+// ! Badge now showing inside of Card, used Button instead.
+// * Clicking on category button should filter out notes of same category -- add this feature later
