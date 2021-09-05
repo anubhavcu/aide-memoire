1f0a8d239757f52c27c4089ea2ae36e02d88826e
anubhavcu <anubhav342y@gmail.com>
2021-09-05 11:17:39 +0530

    added method to login user
    - POST request on route - /api/users/login for login user in
    - used fetch API for making post requests

--- frontend/src/components/screens/LoginScreen/LoginScreen.jsx
+++ frontend/src/components/screens/LoginScreen/LoginScreen.jsx
@@ -1,18 +1,49 @@
 import MainScreen from '../../MainScreen';
 import { Form, Button, Row, Col } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 const LoginScreen = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const res = await fetch('/api/users/login', {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json',
+        },
+        body: JSON.stringify({ email: email, password: password }),
+      });
+      const data = await res.json();
+      console.log(data);
+      localStorage.setItem('userInfo', JSON.stringify(data));
+
+      setLoading(false);
+    } catch (err) {
+      setError(err.response.data.error);
+    }
+    // console.log(email, password);
+  };
   return (
     <div>
       <MainScreen title='LOGIN' children='hello world'>
         <div
           style={{ display: 'flex', flexDirection: 'column', margin: '20px' }}
         >
-          <Form>
+          <Form onSubmit={handleSubmit}>
             <Form.Group className='mb-3' controlId='formBasicEmail'>
               <Form.Label>Email address</Form.Label>
-              <Form.Control type='email' placeholder='Enter email' />
+              <Form.Control
+                type='email'
+                placeholder='Enter email'
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
               <Form.Text className='text-muted'>
                 We'll never share your email with anyone else.
               </Form.Text>
@@ -20,7 +51,12 @@ const LoginScreen = () => {
 
             <Form.Group className='mb-3' controlId='formBasicPassword'>
               <Form.Label>Password</Form.Label>
-              <Form.Control type='password' placeholder='Password' />
+              <Form.Control
+                type='password'
+                placeholder='Password'
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
             </Form.Group>
 
             <Button variant='primary' type='submit'>
