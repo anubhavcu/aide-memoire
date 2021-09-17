e7e1b6c642ad85297227ff533687ec515bf1d12a
anubhavcu <anubhav342y@gmail.com>
2021-09-17 17:43:27 +0530

    necessary changes for production

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const { notFound, errorHandler } = require('./middlewares/errorMiddleware');
+const path = require('path');
 
 const app = express();
 
@@ -12,15 +13,27 @@ const PORT = process.env.PORT || 5000;
 // body parser middleware (to parse data from req.body in POST req's )
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Api is running');
-});
-
 // notes route
 app.use('/api/notes', require('./routes/notesRoutes'));
 
 // users routes
 app.use('/api/users', require('./routes/userRoutes'));
+
+// ===================   deployment  ==========================
+__dirname = path.resolve();
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  });
+} else {
+  app.get('/', (req, res) => {
+    res.send('Api is running');
+  });
+}
+
+// ======================================================
 
 // error handling middlewares
 app.use(notFound);
